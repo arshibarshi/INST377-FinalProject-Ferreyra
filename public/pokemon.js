d1c2b27eeabbc4ce9e855aebafee9b47c349f4a9
Ademir Ferreyra <adefer@terpmail.umd.edu>
2026-05-17 22:12:18 -0400

    added favorites button function

--- public/pokemon.js
+++ public/pokemon.js
@@ -25,11 +25,23 @@ async function allPkmn(){
 
         const typeCell = document.createElement('td');
         typeCell.textContent = pkmnData[index].types;
+
+        const favCell = document.createElement('td');
+        const favBtn = document.createElement('button');
+        favBtn.textContent = '⭐';
+        favBtn.onclick = () => addFavorite(
+        pokemon.name,
+        pkmnData[index].types,
+        pkmnData[index].sprite
+        );
+        favCell.appendChild(favBtn);
+        row.appendChild(favCell);
         
         row.appendChild(imgCell);
         row.appendChild(numCell);
         row.appendChild(nameCell);
         row.appendChild(typeCell);
+        row.appendChild(favCell);
         info.appendChild(row);
     });
 }
@@ -40,6 +52,31 @@ async function getPkmnData(url){
         types: data.types.map(t => t.type.name).join(', '),
         sprite: data.sprites.front_default
     };
+}
+
+async function addFavorite(name, type, image_url){
+    const response = await fetch("/api/favorites", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, type, image_url })
+    });
+    const data = await response.json();
+    console.log('Added to favorites:', data);
+    
+}
+
+async function loadFavorites(){
+    const response = await fetch("/api/favorites");
+    const favorites = await response.json();
+    console.log('Current favorites:', favorites);
+
+    favorites.forEach(pokemon => {
+        const card = document.getElementById('favorites-section');
+        card.innerHTML = '<img src="${pokemon.image_url}" alt="${pokemon.pokemon_name}"> <p>${pokemon.pokemon_name}</p><p>${pokemon.pokemon_type}</p>';
+        favoritesList.appendChild(card);
+    });
 }
 
 
